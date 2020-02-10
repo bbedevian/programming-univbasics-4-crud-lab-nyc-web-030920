4a8568c2c94ba4afb4fb7eb09626146507ff1d2e
Brett Bedevian <brettbedevian@gmail.com>
2020-02-10 21:26:47 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -23,15 +23,15 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-  array.element[index_number]
+  array[index_number]
 end
 
 def retrieve_first_element_from_array(array)
-  array.element[0]
+  array[0]
 end
 
 def retrieve_last_element_from_array(array)
-  array.element[-1]
+  array[-1]
 end
 
 def update_element_from_index(array, index_number, element)
